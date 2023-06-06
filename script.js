70ddec9f7523df12ae941a0e13a3ffcdc26fab05
鈴木響 <h.suzuki@piton.jp>
2023-06-06 21:21:06 +0900

    掲示板

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ function navigateToPage(page) {
 function showAlert(message) {
     window.alert(message);
 }
+
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('message-form');
+const nameInput = document.getElementById('name-input');
+const messageInput = document.getElementById('message-input');
+
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = nameInput.value;
+    const message = messageInput.value;
+
+    if (name.trim() === '' || message.trim() === '') {
+        alert('名前とメッセージを入力してください');
+        return;
+    }
+
+    const timestamp = new Date().toLocaleString();
+    const formattedMessage = `<div><strong>${name}:</strong> ${message}<br><small>${timestamp}</small></div>`;
+    messageContainer.innerHTML += formattedMessage;
+
+    nameInput.value = '';
+    messageInput.value = '';
+});
