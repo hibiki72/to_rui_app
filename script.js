b367a6d51ddc1aa0d8c883591bd2c51bccd6c38f
鈴木響 <h.suzuki@piton.jp>
2023-06-07 10:10:59 +0900

    ログイン機能

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-function navigateToPage(page) {
-    window.location.href = page;
-}
 
-function showAlert(message) {
-    window.alert(message);
-}
+// ログインフォームのサブミットイベントリスナー
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('login-form').addEventListener('submit', function(event) {
+
+        event.preventDefault();
+        const username = document.getElementById('username-input').value;
+        const password = document.getElementById('password-input').value;
+
+        // DBがないので、一旦決め打ち
+        if (username === 'admin' && password === 'password') {
+            sessionStorage.setItem('username', username);
+            window.location.href = 'index.html'; // ログイン成功時のページ遷移
+        } else {
+            window.alert('ログインに失敗しました。ユーザー名とパスワードを確認してください。');
+        }
+    });
+}, false);
+
+// 新規アカウント作成フォームのサブミットイベントリスナー
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('signup-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const username = document.getElementById('username-input').value;
+        const password = document.getElementById('password-input').value;
+        
+        // DBにusername,passwordを登録する想定
+        
+        alert('新規アカウントが作成されました。ログインしてください。');
+        window.location.href = 'login.html'; // 新規アカウント作成後のページ遷移（ログイン画面へ）
+    });
+}, false);
 
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('message-form');
